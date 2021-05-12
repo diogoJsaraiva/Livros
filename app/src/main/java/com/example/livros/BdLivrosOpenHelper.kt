e49f616a0db1c90e2a914c33c2006293358ddbf4
1703703 <diogo.saraiva12@hotmail.com>
2021-05-12 10:33:25 +0100

    Cirar o openHelper para abrir a base de dados de livros

---
+++ app/src/main/java/com/example/livros/BdLivrosOpenHelper.kt
@@ -0,0 +1,20 @@
+package com.example.livros
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BdLivrosOpenHelper(context: Context?
+) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
+    override fun onCreate(db: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+    companion object{
+        const val NOME_BASE_DADOS = "livros.db"
+        const val VERSAO_BASE_DADOS = 1
+    }
+
+}
