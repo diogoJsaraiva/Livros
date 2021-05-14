c4bff1a189e8347cbaa54d3d6f12e78fb9543a85
1703703 <diogo.saraiva12@hotmail.com>
2021-05-14 10:10:56 +0100

    Cirar a tabela livro

--- app/src/main/java/com/example/livros/BdLivrosOpenHelper.kt
+++ app/src/main/java/com/example/livros/BdLivrosOpenHelper.kt
@@ -7,8 +7,16 @@ import android.database.sqlite.SQLiteOpenHelper
 class BdLivrosOpenHelper(context: Context?
 ) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
     override fun onCreate(db: SQLiteDatabase?) {
-        TabelaCategorias(db).cria()
+        if (db != null) {
+            TabelaCategorias(db).cria()
+            TabelaLivro(db).cria()
+        }
     }
+
+
+
+
+
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
     }
