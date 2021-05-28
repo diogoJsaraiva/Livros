c7d78279ddeef5c173273027af5cf40d3bfe9f5d
1703703 <diogo.saraiva12@hotmail.com>
2021-05-28 10:21:34 +0100

    corrigir test consegueInserirLivros

--- app/src/androidTest/java/com/example/livros/TestBaseDados.kt
+++ app/src/androidTest/java/com/example/livros/TestBaseDados.kt
@@ -165,7 +165,7 @@ class TestBaseDados {
 
         livro.id = inserirLivro(tabelaLivro,livro)
 
-        assertEquals(livro,getLivroBaseDados(tabelaLivro,categoria.id))
+        assertEquals(livro,getLivroBaseDados(tabelaLivro,livro.id))
 
 
         db.close()
